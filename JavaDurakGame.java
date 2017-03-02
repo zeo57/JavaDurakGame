62d2ffdd1b9edae481a951fd841c97f2dbfc6cbb
zeo57 <dmdovydassis@gmail.com>
2017-03-02 09:34:59 +0200

    Update JavaDurakGame.java

--- JavaDurakGame.java
+++ JavaDurakGame.java
@@ -22,9 +22,9 @@ class JavaDurakGame
       System.out.println(FileLinesCount());
       System.out.println(OffencePlayerCards(1, FindVerticalLine(1)));
       System.out.println(DefencePlayerCards(1, FindVerticalLine(1)));
-      System.out.println((StringLineConverterToCharArray(DefencePlayerCards(1, FindVerticalLine(1)))));
-      System.out.println(branchIfPlayerHaveNonTrumpCard((StringLineConverterToCharArray(DefencePlayerCards(1, FindVerticalLine(1)))), TrumpCard) );
-
+      System.out.println((StringLineConverterToCharArray(OffencePlayerCards(1, FindVerticalLine(1)))));
+      System.out.println(branchIfPlayerHaveNonTrumpCard((StringLineConverterToCharArray(OffencePlayerCards(1, FindVerticalLine(1)))), TrumpCard) );
+      getMinValue((StringLineConverterToCharArray(OffencePlayerCards(1, FindVerticalLine(1)))), TrumpCard);
   }
 
     public static String[] FileRead()
@@ -112,7 +112,7 @@ class JavaDurakGame
         String[] array = FileRead();
         String loadingArray;
 
-        loadingArray =  array[lineNumber].substring(0, (array[lineNumber].length()-(verticalLinePosition + 2)));
+        loadingArray =  array[lineNumber].substring(0, ((array[lineNumber].length()-1) -(verticalLinePosition + 2)));
 
         return loadingArray;
     }
@@ -199,6 +199,128 @@ class JavaDurakGame
      }
 
 
+
+     public static int getMinValue(char[] playerArray, char TrumpCard)
+      {
+        int [] rankIntArray;
+        String [] rankStringArray;
+
+        int cardsNumber = 0;
+         rankIntArray = new int[playerArray.length];
+         rankStringArray = new String[playerArray.length];
+
+
+
+         for (int i = 0 ; i < playerArray.length; i++)
+         {
+             if(playerArray[i] != ' ')
+             {
+
+               cardsNumber ++;
+             }
+         }
+
+
+         for (int i = 0 ; i < playerArray.length; i++)
+         {
+            System.out.print(playerArray[i]);
+         }
+
+          for (int i = 0; i < playerArray.length; i++)
+          {
+             rankStringArray[i]  = String.valueOf(playerArray[i]);
+
+          }
+
+
+          for (int i = 0; i <= cardsNumber+2; i = i+3)
+          {
+
+            if (!rankStringArray[i].equals(String.valueOf(TrumpCard)) && (!rankStringArray[i].equals(String.valueOf(' ')) ))
+                {
+                  if (rankStringArray[i+1].equals("A"))
+                  {
+                   rankStringArray[i+1] = "14";
+                  }
+
+
+                  if (rankStringArray[i+1].equals("K"))
+                  {
+                   rankStringArray[i+1] = "13";
+                  }
+
+                  if(rankStringArray[i+1].equals("Q"))
+                  {
+                    rankStringArray[i+1] = "12";
+                  }
+
+                  if(rankStringArray[i+1].equals("J"))
+                  {
+                    rankStringArray[i+1] = "11";
+                  }
+
+                  if(rankStringArray[i+1].equals("T"))
+                  {
+                   rankStringArray[i+1] = "10";
+                  }
+
+
+               }
+
+
+
+
+
+           }
+
+
+
+
+
+
+
+
+
+        System.out.println();
+        for (int i = 0; i < playerArray.length; i++)
+        {
+          System.out.print(rankStringArray[i]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //make return minimumvalue of nontrumpcards
+
+
+
+
+
+        int minValue = Integer.parseInt("5") ;
+
+
+/*
+        for (int i = 1; i < array.length; i++) {
+
+        if (array[i] < minValue) {
+            minValue = array[i];
+        }
+    }*/
+    return minValue;
+}
+
+
      //static method optimize
 
 
@@ -206,5 +328,3 @@ class JavaDurakGame
 
 
 
-
-}
