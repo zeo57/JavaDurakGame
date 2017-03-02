4b2f1129982ecf9f71d0b53102ffe05c8ad4f452
zeo57 <dmdovydassis@gmail.com>
2017-03-02 03:21:41 +0200

    Update JavaDurakGame.java

--- JavaDurakGame.java
+++ JavaDurakGame.java
@@ -4,61 +4,75 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
-
-
-
 class JavaDurakGame
 {
-
-  private static final String FilePathName = "/home/zeo57/Desktop/Stupid/Data.txt";
+  private static final String FilePathName = "/home/zeo57/Desktop/Stupid/Data.txt"; //Linux directory 
 
   public static void main(String[] args)
   {
-
-          getFirsPlayerCardArray();
-
+      char TrumpCard = GetTrumpCard();
+      System.out.print(TrumpCard);
+      System.out.println(FindVerticalLine(1));
   }
 
     public static String[] FileRead()
     {
-      int i = 0,h =0;
+        int i = 0,h =0;
         String [] LinesArray;
         LinesArray = new String[2];
         try (BufferedReader br = new BufferedReader(new FileReader(FilePathName)))
         {
-
-			  String sCurrentLine;
-
-			  while ((sCurrentLine = br.readLine()) != null)
-        {
-
-            LinesArray[i] =(sCurrentLine);
-            h= h+1;// h means lines
-          i++;
-        }
-
-		    } catch (IOException e)
+	    String sCurrentLine;
+		
+            while ((sCurrentLine = br.readLine()) != null)
+           {
+                LinesArray[i] =(sCurrentLine);
+                h= h+1;// h means lines
+                i++;
+            }
+          } 
+	    catch (IOException e)
          {
-			e.printStackTrace();
-		    }
+	     e.printStackTrace();
+         }
 
-        //reading file change to char
-        return LinesArray;
+   
+          System.out.println(LinesArray[1].substring(kintamasis, LinesArray[1].length()));
+          
+	    return LinesArray;
 	}
 
 
-    public static  char getFirsPlayerCardArray()
+    public static  char GetTrumpCard()
     {
-        char TrumpCard =' ';
-
         String[] testing = FileRead();
-         System.out.println(Arrays.toString(testing));
-         System.out.println(testing[0]);
-         System.out.println(testing[1]);
+        char TrumpCard = testing[0].charAt(0);
 
+        return TrumpCard;
+    }
 
-         return TrumpCard;
+    public static int FindVerticalLine(int LineNumber)
+    {
+        String[]  Array = FileRead();
+        int kintamasis = (Array[LineNumber].indexOf("|"));
+
+        return kintamasis;
     }
 
 
+
+/*
+    public static String[] OffencePlayerCards()
+    {
+
+    }
+*/
+
+
+
+
+
+
+
 }
+
